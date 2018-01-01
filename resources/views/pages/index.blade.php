a4c727a062e465f852615625288c6184e09b73ff
Nikolay Popov <koyabica@gmail.com>
2018-01-01 17:22:47 +0300

    Create Comment Model

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>PAGE</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="manifest" href="site.webmanifest">
+        <link rel="apple-touch-icon" href="icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        <!--[if lte IE 9]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+        <article>
+
+        	<h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
+
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce in malesuada dui, ac sagittis nisi. Mauris faucibus congue orci ac elementum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In leo diam, hendrerit ut diam ac, consequat pellentesque lectus. Duis ante nulla, mattis nec lacus vel, gravida blandit nisl. Morbi ut nisl sed purus vehicula pulvinar. Maecenas maximus mollis pellentesque. Donec fermentum condimentum nisl, a convallis orci auctor non. Ut vehicula risus dolor, id mattis ante lobortis hendrerit.</p>
+			<p>Morbi eget dolor non tellus facilisis iaculis non dictum lorem. Suspendisse sed massa et magna iaculis luctus non consequat est. Aenean sed libero purus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nunc vulputate arcu fringilla eleifend sagittis. Integer scelerisque odio pharetra, semper orci non, semper ante. Suspendisse convallis arcu et dapibus eleifend. Integer venenatis ligula et felis dictum, a ullamcorper magna gravida. Ut tortor felis, interdum ac dui sed, laoreet dictum dolor. Nunc rhoncus pulvinar libero in molestie.</p>
+
+		</article>
+
+		
+
+		<h3>Comments</h3>
+
+
+        
+
+
+
+
+        <script src="js/vendor/modernizr-3.5.0.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+        <script>
+            window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
+            ga('create','UA-XXXXX-Y','auto');ga('send','pageview')
+        </script>
+        <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+    </body>
+</html>
